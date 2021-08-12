1009aa4e3ee7aa834f3a43a5966d6876a3edad18
wsbky <wsbkye@gmail.com>
2021-08-12 16:14:16 +0900

    [add] operator implementation

--- src/bigint.cc
+++ src/bigint.cc
@@ -1,8 +1,5 @@
 #include "bigint.hh"
 
-#include <cmath>
-#include <sstream>
-
 namespace extypes {
 
 bigint::bigint() : pos(true) {}
@@ -13,7 +10,7 @@ bigint::bigint(int n) : pos(n >= 0) {
         n /= base;
     }
 }
-bigint::bigint(unsigned n) : pos(true) {
+bigint::bigint(unsigned n, bool p = true) : pos(p) {
     while (n != 0) {
         num.push_back(n % base);
         n /= base;
@@ -26,11 +23,222 @@ bigint::bigint(long n) : pos(n >= 0) {
         n /= base;
     }
 }
-bigint::bigint(unsigned long n) : pos(true) {
+bigint::bigint(unsigned long n, bool p = true) : pos(p) {
     while (n != 0) {
         num.push_back(n % base);
         n /= base;
     }
 }
+bigint::bigint(long long n) : pos(n >= 0) {
+    n = std::abs(n);
+    while (n != 0) {
+        num.push_back(n % base);
+        n /= base;
+    }
+}
+bigint::bigint(std::vector<unsigned> a, bool p = true) : num(a), pos(p) {}
+bigint::bigint(const bigint &a) : num(a.num), pos(a.pos) {}
+
+bool bigint::sign() const { return pos; }
+bigint bigint::abs() const { return bigint(num, true); }
+// TODO: fact
+// TODO: digits
+
+// TODO: input & output
+std::string bigint::to_string() const {
+    std::ostringstream ros;
+    if (num.size() == 0) return "NaN";
+    if (!pos) ros << "-";
+    ros << num.at(num.size() - 1);
+    if (num.size() >= 1)
+        std::for_each(num.rbegin() + 1, num.rend(), [&](const auto &e) {
+            ros << std::setfill('0') << std::setw(base_len - 1) << e;
+        });
+    return ros.str();
+}
+std::ostream &operator<<(std::ostream &os, const bigint &a) {
+    return os << a.to_string();
+}
+
+// TODO: casting operators
+
+bigint bigint::operator+() const { return *this; }
+bigint bigint::operator-() const { return bigint(num, !pos); }
+
+unsigned bigint::operator[](int a) const { return num.at(a); }
+
+bigint &bigint::operator+=(const bigint &a) {
+    if (pos ^ a.pos) return *this -= -a;
+
+    bigint t(a);
+    unsigned moveup = 0;
+    if (num.size() < a.num.size()) {
+        t = *this;
+        *this = a;
+        t.num.resize(num.size(), 0);
+    }
+
+    auto it_t = t.num.begin();
+    for (auto &e : num) {
+        e += (*it_t++) + moveup;
+        moveup = (e >= base) ? 1 : 0;
+        e -= base;
+    }
+    if (moveup) num.push_back(moveup);
+    return *this;
+}
+bigint &bigint::operator+=(int a) { return *this += bigint(a); }
+bigint &bigint::operator+=(unsigned a) { return *this += bigint(a); }
+bigint &bigint::operator+=(long a) { return *this += bigint(a); }
+bigint &bigint::operator+=(unsigned long a) { return *this += bigint(a); }
+
+bigint &bigint::operator-=(const bigint &a) {
+    if (pos ^ a.pos) return *this += -a;
+
+    bigint t(a);
+    unsigned moveup = 0;
+    bool rev_flag = false;
+    if ((*this).abs() < a.abs()) {
+        rev_flag = true;
+        t = *this;
+        *this = a;
+        if (num.size() > t.num.size()) t.num.resize(num.size(), 0);
+    }
+
+    auto it_t = t.num.begin();
+    for (auto &e : num) {
+        e += base - (*it_t++) + moveup;
+        if (e >= base) {
+            e -= base;
+            moveup = 0;
+        } else
+            moveup = -1;
+    }
+    if (num.at(num.size() - 1) == 0) num.pop_back();
+
+    if (rev_flag ^ pos) pos = !pos;
+    return *this;
+}
+bigint &bigint::operator-=(int a) { return *this -= bigint(a); }
+bigint &bigint::operator-=(unsigned a) { return *this -= bigint(a); }
+bigint &bigint::operator-=(long a) { return *this -= bigint(a); }
+bigint &bigint::operator-=(unsigned long a) { return *this -= bigint(a); }
+
+bigint &bigint::operator*=(const bigint &a) {
+    return (*this).multiplication_a(a);
+}
+bigint &bigint::operator*=(int a) { return *this *= bigint(a); }
+bigint &bigint::operator*=(unsigned a) { return *this *= bigint(a); }
+bigint &bigint::operator*=(long a) { return *this *= bigint(a); }
+bigint &bigint::operator*=(unsigned long a) { return *this *= bigint(a); }
+
+bigint &bigint::operator/=(const bigint &a) { return (*this).division_a(a); }
+bigint &bigint::operator/=(int a) { return *this /= bigint(a); }
+bigint &bigint::operator/=(unsigned a) { return *this /= bigint(a); }
+bigint &bigint::operator/=(long a) { return *this /= bigint(a); }
+bigint &bigint::operator/=(unsigned long a) { return *this /= bigint(a); }
+
+bigint bigint::operator*(const bigint &a) const {
+    bigint t(*this);
+    return (t *= a);
+}
+bigint bigint::operator*(int a) const {
+    bigint t(*this);
+    return (t *= a);
+}
+bigint bigint::operator*(unsigned a) const {
+    bigint t(*this);
+    return (t *= a);
+}
+bigint bigint::operator*(long a) const {
+    bigint t(*this);
+    return (t *= a);
+}
+bigint bigint::operator*(unsigned long a) const {
+    bigint t(*this);
+    return (t *= a);
+}
+
+bigint &bigint::operator=(const bigint &a) {
+    num = a.num;
+    pos = a.pos;
+    return *this;
+}
+
+bool bigint::operator==(const bigint &a) const {
+    return (pos == a.pos) && (num.size() == a.num.size()) &&
+           std::equal(num.cbegin(), num.cend(), a.num.cbegin());
+}
+bool bigint::operator==(int a) const { return *this == bigint(a); }
+bool bigint::operator==(unsigned a) const { return *this == bigint(a); }
+bool bigint::operator==(long a) const { return *this == bigint(a); }
+bool bigint::operator==(unsigned long a) const { return *this == bigint(a); }
+
+bool bigint::operator>(const bigint &a) const {
+    if (pos ^ a.pos) {
+        if (pos) return true;
+        else
+            return false;
+    }
+    if (num.size() > a.num.size()) return true;
+    if (num.size() < a.num.size()) return false;
+    for (int i = num.size() - 1; i >= 0; --i) {
+        if (num.at(i) > a.num.at(i)) return true;
+        if (num.at(i) < a.num.at(i)) return false;
+    }
+    return false;
+}
+bool bigint::operator>(int a) const { return *this > bigint(a); }
+bool bigint::operator>(unsigned a) const { return *this > bigint(a); }
+bool bigint::operator>(long a) const { return *this > bigint(a); }
+bool bigint::operator>(unsigned long a) const { return *this > bigint(a); }
+bool operator>(int a, const bigint &b) { return b < bigint(a); }
+bool operator>(unsigned a, const bigint &b) { return b < bigint(a); }
+bool operator>(long a, const bigint &b) { return b < bigint(a); }
+bool operator>(unsigned long a, const bigint &b) { return b < bigint(a); }
+
+bool bigint::operator>=(const bigint &a) const { return !(*this < a); }
+bool bigint::operator>=(int a) const { return *this >= bigint(a); }
+bool bigint::operator>=(unsigned a) const { return *this >= bigint(a); }
+bool bigint::operator>=(long a) const { return *this >= bigint(a); }
+bool bigint::operator>=(unsigned long a) const { return *this >= bigint(a); }
+bool operator>=(int a, const bigint &b) { return b <= bigint(a); }
+bool operator>=(unsigned a, const bigint &b) { return b <= bigint(a); }
+bool operator>=(long a, const bigint &b) { return b <= bigint(a); }
+bool operator>=(unsigned long a, const bigint &b) { return b <= bigint(a); }
+
+bool bigint::operator<(const bigint &a) const {
+    if (pos ^ a.pos) {
+        if (pos) return false;
+        else
+            return true;
+    }
+    if (num.size() > a.num.size()) return false;
+    if (num.size() < a.num.size()) return true;
+    for (auto i = num.size() - 1;; --i) {
+        if (num.at(i) > a.num.at(i)) return false;
+        if (num.at(i) < a.num.at(i)) return true;
+        if (i == 0) break;
+    }
+    return false;
+}
+bool bigint::operator<(int a) const { return *this < bigint(a); }
+bool bigint::operator<(unsigned a) const { return *this < bigint(a); }
+bool bigint::operator<(long a) const { return *this < bigint(a); }
+bool bigint::operator<(unsigned long a) const { return *this < bigint(a); }
+bool operator<(int a, const bigint &b) { return b > bigint(a); }
+bool operator<(unsigned a, const bigint &b) { return b > bigint(a); }
+bool operator<(long a, const bigint &b) { return b > bigint(a); }
+bool operator<(unsigned long a, const bigint &b) { return b > bigint(a); }
+
+bool bigint::operator<=(const bigint &a) const { return !(*this > a); }
+bool bigint::operator<=(int a) const { return *this <= bigint(a); }
+bool bigint::operator<=(unsigned a) const { return *this <= bigint(a); }
+bool bigint::operator<=(long a) const { return *this <= bigint(a); }
+bool bigint::operator<=(unsigned long a) const { return *this <= bigint(a); }
+bool operator<=(int a, const bigint &b) { return b >= bigint(a); }
+bool operator<=(unsigned a, const bigint &b) { return b >= bigint(a); }
+bool operator<=(long a, const bigint &b) { return b >= bigint(a); }
+bool operator<=(unsigned long a, const bigint &b) { return b >= bigint(a); }
 
 }  // namespace extypes
--- src/bigint.hh
+++ src/bigint.hh
@@ -1,18 +1,232 @@
 #pragma once
 
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
 
 namespace extypes {
 struct bigint {
     bigint();
     bigint(int);
-    bigint(unsigned);
+    bigint(unsigned, bool);
     bigint(long);
-    bigint(unsigned long);
+    bigint(unsigned long, bool);
+    bigint(long long);
+    bigint(std::vector<unsigned>, bool);
+    bigint(const bigint &);
+
+    bool sign() const;
+    bigint abs() const;
+    bigint fact() const;
+    std::string::size_type digits() const;
+
+    std::string to_string() const;
+    friend std::ostream &operator<<(std::ostream &, const bigint &);
+    friend std::istream &operator>>(std::istream &, const bigint &);
+
+    operator int() const;
+    operator unsigned() const;
+    operator long() const;
+    operator unsigned long() const;
+
+    bigint operator+() const;
+    bigint operator-() const;
+
+    unsigned operator[](int) const;
+
+    bigint &operator+=(const bigint &);
+    bigint &operator+=(int);
+    bigint &operator+=(unsigned);
+    bigint &operator+=(long);
+    bigint &operator+=(unsigned long);
+
+    bigint &operator-=(const bigint &);
+    bigint &operator-=(int);
+    bigint &operator-=(unsigned);
+    bigint &operator-=(long);
+    bigint &operator-=(unsigned long);
+
+    bigint &operator*=(const bigint &);
+    bigint &operator*=(int);
+    bigint &operator*=(unsigned);
+    bigint &operator*=(long);
+    bigint &operator*=(unsigned long);
+
+    bigint &operator/=(const bigint &);
+    bigint &operator/=(int);
+    bigint &operator/=(unsigned);
+    bigint &operator/=(long);
+    bigint &operator/=(unsigned long);
+
+    bigint &operator%=(const bigint &);
+    bigint &operator%=(int);
+    bigint &operator%=(unsigned);
+    bigint &operator%=(long);
+    bigint &operator%=(unsigned long);
+
+    bigint operator+(const bigint &) const;
+    bigint operator+(int) const;
+    bigint operator+(unsigned) const;
+    bigint operator+(long) const;
+    bigint operator+(unsigned long) const;
+    friend bigint operator+(int, const bigint &);
+    friend bigint operator+(unsigned, const bigint &);
+    friend bigint operator+(long, const bigint &);
+    friend bigint operator+(unsigned long, const bigint &);
+
+    bigint operator-(const bigint &) const;
+    bigint operator-(int) const;
+    bigint operator-(unsigned) const;
+    bigint operator-(long) const;
+    bigint operator-(unsigned long) const;
+    friend bigint operator-(int, const bigint &);
+    friend bigint operator-(unsigned, const bigint &);
+    friend bigint operator-(long, const bigint &);
+    friend bigint operator-(unsigned long, const bigint &);
+
+    bigint operator*(const bigint &) const;
+    bigint operator*(int) const;
+    bigint operator*(unsigned) const;
+    bigint operator*(long) const;
+    bigint operator*(unsigned long) const;
+    friend bigint operator*(int, const bigint &);
+    friend bigint operator*(unsigned, const bigint &);
+    friend bigint operator*(long, const bigint &);
+    friend bigint operator*(unsigned long, const bigint &);
+
+    bigint operator/(const bigint &) const;
+    bigint operator/(int) const;
+    bigint operator/(unsigned) const;
+    bigint operator/(long) const;
+    bigint operator/(unsigned long) const;
+    friend bigint operator/(int, const bigint &);
+    friend bigint operator/(unsigned, const bigint &);
+    friend bigint operator/(long, const bigint &);
+    friend bigint operator/(unsigned long, const bigint &);
+
+    bigint operator%(const bigint &) const;
+    bigint operator%(int) const;
+    bigint operator%(unsigned) const;
+    bigint operator%(long) const;
+    bigint operator%(unsigned long) const;
+    friend bigint operator%(int, const bigint &);
+    friend bigint operator%(unsigned, const bigint &);
+    friend bigint operator%(long, const bigint &);
+    friend bigint operator%(unsigned long, const bigint &);
+
+    bigint &operator=(const bigint &);
+    bigint &operator=(int);
+    bigint &operator=(unsigned);
+    bigint &operator=(long);
+    bigint &operator=(unsigned long);
+
+    bool operator==(const bigint &) const;
+    bool operator==(int) const;
+    bool operator==(unsigned) const;
+    bool operator==(long) const;
+    bool operator==(unsigned long) const;
+
+    bool operator>(const bigint &) const;
+    bool operator>(int) const;
+    bool operator>(unsigned) const;
+    bool operator>(long) const;
+    bool operator>(unsigned long) const;
+    friend bool operator>(int, const bigint &);
+    friend bool operator>(unsigned, const bigint &);
+    friend bool operator>(long, const bigint &);
+    friend bool operator>(unsigned long, const bigint &);
+
+    bool operator>=(const bigint &) const;
+    bool operator>=(int) const;
+    bool operator>=(unsigned) const;
+    bool operator>=(long) const;
+    bool operator>=(unsigned long) const;
+    friend bool operator>=(int, const bigint &);
+    friend bool operator>=(unsigned, const bigint &);
+    friend bool operator>=(long, const bigint &);
+    friend bool operator>=(unsigned long, const bigint &);
+
+    bool operator<(const bigint &) const;
+    bool operator<(int) const;
+    bool operator<(unsigned) const;
+    bool operator<(long) const;
+    bool operator<(unsigned long) const;
+    friend bool operator<(int, const bigint &);
+    friend bool operator<(unsigned, const bigint &);
+    friend bool operator<(long, const bigint &);
+    friend bool operator<(unsigned long, const bigint &);
+
+    bool operator<=(const bigint &) const;
+    bool operator<=(int) const;
+    bool operator<=(unsigned) const;
+    bool operator<=(long) const;
+    bool operator<=(unsigned long) const;
+    friend bool operator<=(int, const bigint &);
+    friend bool operator<=(unsigned, const bigint &);
+    friend bool operator<=(long, const bigint &);
+    friend bool operator<=(unsigned long, const bigint &);
+
+    friend bigint pow(const bigint &, int);
+    friend bigint abs(const bigint &);
 
   private:
     std::vector<unsigned> num;
     bool pos;
     const static unsigned base = 1000000000;
+    const static unsigned short base_len = 10;
+
+    bigint &multiplication_a(const bigint &a) {
+        bigint t(*this);
+        auto min_dim = t.num.size() + a.num.size() - 2;
+        num.clear();
+        num.resize(min_dim + 1);
+        for (auto it_t = t.num.begin(); it_t != t.num.end(); ++it_t)
+            for (auto it_a = a.num.begin(); it_a != a.num.end(); ++it_a) {
+                auto dim = (it_a - a.num.begin()) + (it_t - t.num.begin());
+                unsigned long long tmp =
+                    (unsigned long long)*it_t * (unsigned long long)*it_a;
+                for (int i = 0; tmp != 0; ++i) {
+                    if (dim + (unsigned long)i < num.size()) {
+                        tmp += num.at(dim + i);
+                        num.at(dim + i) = tmp % base;
+                    } else
+                        num.push_back(tmp % base);
+                    tmp /= base;
+                }
+            }
+        return *this;
+    }
+
+    bigint &division_a(const bigint &a) {
+        if ((*this).abs() < a.abs()) {
+            num.clear();
+            num.push_back(0);
+            return *this;
+        };
+        if ((*this).abs() == a.abs()) {
+            num.clear();
+            num.push_back(1);
+            pos ^= !a.pos;
+            return *this;
+        }
+        auto div_t = num.size() - a.num.size() + 1;
+        bigint t(*this);
+        num.clear();
+        num.resize(div_t, 0);
+        auto ot_size = t.num.size();
+        for (auto i = ot_size - 1; i >= ot_size - div_t; --i) {
+            unsigned tmp = *(t.num.begin() + i);
+            if (i != ot_size - 1) tmp += *(t.num.begin() + i + 1);
+            unsigned q = tmp / *(a.num.end() - 1);
+            t -= a * q;
+        }
+        return *this;
+    }
 };
+
+bigint factorial(int);
 }  // namespace extypes
